4607f201b86895452e05e4de9fcdface1e909494
arcxnjodev <railsonmoreirah38@gmail.com>
2026-04-23 23:18:28 -0400

    Corrige exports e remove imports não usados

--- src/Components/AdminPanel.tsx
+++ src/Components/AdminPanel.tsx
@@ -1,38 +1,29 @@
-import Logo from "../../assets/images/logo.webp";
-import { useDispatch } from "react-redux";
-import { exitUser } from "../../Store/userSlice";
+import { AdminMenu } from "./Ui/AdminMenu";
+import { Settings } from "./Ui/Settings";
+import { useSelector } from "react-redux";
+import { userSliceType } from "../Store/userSlice";
+import { useNavigate } from "react-router-dom";
+import { useEffect } from "react";
 
-type propsType = {
-  email: string;
-};
+export const AdminPanel = () => {
+  const navigate = useNavigate();
+  const { email } = useSelector((store: { user: userSliceType }) => store.user);
+  const token = localStorage.getItem("token");
 
-export const AdminMenu = (props: propsType) => {
-  const dispatch = useDispatch();
+  useEffect(() => {
+    if (!token && !email) {
+      navigate("/login");
+    }
+  }, [token, email, navigate]);
 
-  const handleLogout = () => {
-    dispatch(exitUser());
-    window.location.href = "/login";
-  };
+  if (!token && !email) {
+    return null;
+  }
 
   return (
-    <div className="sticky h-[100vh] w-[250px] bg-slate-600 text-white">
-      <div className="flex flex-col items-center">
-        <div className="text-white flex items-center mt-5">
-          <img src={Logo} className="w-[60px]" />
-          <p className="font-bold text-xl">Admin Panel</p>
-        </div>
-        <div>
-          <p className="mt-5 break-all px-2 text-center">{props.email}</p>
-        </div>
-        <div className="w-full p-2 mt-[100px]">
-          <button
-            onClick={handleLogout}
-            className="bg-slate-800 w-full inline-block text-center rounded-lg p-2 mt-1 cursor-pointer hover:bg-slate-700"
-          >
-            Logout
-          </button>
-        </div>
-      </div>
+    <div className="flex">
+      <AdminMenu email={email || ""} />
+      <Settings />
     </div>
   );
 };
