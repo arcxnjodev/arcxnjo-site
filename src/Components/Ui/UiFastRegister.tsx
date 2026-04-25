9f46e8027c0d63f88ab0cd774c81de2c3b2fb044
arcxnjodev <railsonmoreirah38@gmail.com>
2026-04-25 14:46:47 -0400

    Altera texto do registro rápido para arcxnjo.com

--- src/Components/Ui/UiFastRegister.tsx
+++ src/Components/Ui/UiFastRegister.tsx
@@ -1,11 +1,12 @@
 import { useState } from "react";
 import { Link } from "react-router-dom";
+
 export const UiFastRegister = () => {
   const [username, setUsername] = useState<string>("")
   return (
     <div className="flex items-center mt-[50px]">
-      <div className="relative flex items-center bg-gray-800 rounded-lg w-[250px] h-[50px] text-white focus:outline-purple-700">
-        <p className="absolute text-md left-2 w-1/2">banana.com/</p>
+      <div className="relative flex items-center bg-gray-800 rounded-lg w-[280px] h-[50px] text-white focus:outline-purple-700">
+        <p className="absolute text-md left-2 w-1/2">Arcxnjo.com/</p>
         <input
           type="text"
           className="w-1/2 rounded border-none bg-transparent ml-auto text-gray-300 outline-none"
