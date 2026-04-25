cd3a3d2d6bc76f5b7b4ab6b1f7d15145e6e9b265
arcxnjodev <railsonmoreirah38@gmail.com>
2026-04-24 20:30:57 -0400

    CORS aberto para qualquer origem

--- backend/server.js
+++ backend/server.js
@@ -8,12 +8,18 @@ const { Pool } = require('pg');
 const fs = require('fs');
 
 const app = express();
-app.use(cors({
-  origin: '*',
-  credentials: true,
-  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-  allowedHeaders: ['Content-Type', 'Authorization']
-}));
+// CORS completamente aberto para testes
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Credentials', 'true');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+  
+  if (req.method === 'OPTIONS') {
+    return res.sendStatus(200);
+  }
+  next();
+});
 app.use(express.json());
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 
