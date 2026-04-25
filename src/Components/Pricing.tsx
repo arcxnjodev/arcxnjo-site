b9e3bf055411d809c333c13a7af78fd6878b2b3f
arcxnjodev <railsonmoreirah38@gmail.com>
2026-04-25 16:42:14 -0400

    Corrige botão Get Started para redirecionar para registro

--- src/Components/Pricing.tsx
+++ src/Components/Pricing.tsx
@@ -1,3 +1,5 @@
+import { Link } from "react-router-dom";
+
 export const Pricing = () => {
   return (
     <div className="text-white w-full flex flex-col items-center mt-[100px] pb-[200px]">
@@ -62,7 +64,9 @@ export const Pricing = () => {
               <p>Add Your Social Media</p>
             </div>
           </div>
-          <button className="bg-purple-700 rounded-lg px-4 py-2">Get Started</button>
+          <Link to="/register" className="bg-purple-700 rounded-lg px-4 py-2 inline-block text-center">
+            Get Started
+          </Link>
         </div>
 
         <div className="bg-gray-900 p-5 rounded-xl flex flex-col gap-4 w-[370px] bg-main">
@@ -180,7 +184,9 @@ export const Pricing = () => {
               <p>badges+</p>
             </div>
           </div>
-          <button className="bg-purple-700 rounded-lg px-4 py-2">Get Started</button>
+          <Link to="/register" className="bg-purple-600 rounded-lg px-4 py-2 inline-block text-center">
+            Get Started
+          </Link>
         </div>
       </div>
     </div>
