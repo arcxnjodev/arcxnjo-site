b2f8af4df75f360093f71526ec9654f3126243c5
arcxnjo <railsonmoreirah38@gmail.com>
2026-06-27 11:25:24 -0400

    fix: bypass self-signed SSL certificate validation on local environment

--- server.js
+++ server.js
@@ -1,3 +1,6 @@
+require('dotenv').config();
+process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0';
+
 const axios = require('axios');
 const express = require('express');
 const bcrypt = require('bcrypt');
