786412924d87686ae8cd8eb5f19880f5137f2198
arcxnjo <railsonmoreirah38@gmail.com>
2026-06-27 12:07:41 -0400

    fix: resolve credentials origin mismatch in CORS middleware

--- server.js
+++ server.js
@@ -23,7 +23,14 @@ cloudinary.config({
 });
 
 app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*');
+  const origin = req.headers.origin;
+  
+  if (origin) {
+    res.header('Access-Control-Allow-Origin', origin);
+  } else {
+    res.header('Access-Control-Allow-Origin', '*');
+  }
+  
   res.header('Access-Control-Allow-Credentials', 'true');
   res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
   res.header(
